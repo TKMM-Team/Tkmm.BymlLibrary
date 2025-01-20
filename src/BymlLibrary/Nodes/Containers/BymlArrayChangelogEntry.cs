5ffadff4b241e2d3948b55218d17b90fc6c456f2
ArchLeaders <archleadership28@gmail.com>
2025-01-20 00:38:56 -0800

    Add implicit cast without key

--- src/BymlLibrary/Nodes/Containers/BymlArrayChangelogEntry.cs
+++ src/BymlLibrary/Nodes/Containers/BymlArrayChangelogEntry.cs
@@ -12,12 +12,18 @@ public readonly struct BymlArrayChangelogEntry
 
     public Byml? KeySecondary { get; init; }
 
-    public static implicit operator BymlArrayChangelogEntry((int index, BymlChangeType change, Byml node, Byml? keyPrimary, Byml? keySecondary) x) => new() {
-        Index = x.index,
-        Change = x.change,
-        Node = x.node,
-        KeyPrimary = x.keyPrimary,
-        KeySecondary = x.keySecondary,
+    public static implicit operator BymlArrayChangelogEntry((int index, BymlChangeType change, Byml node, Byml? keyPrimary, Byml? keySecondary) src) => new() {
+        Index = src.index,
+        Change = src.change,
+        Node = src.node,
+        KeyPrimary = src.keyPrimary,
+        KeySecondary = src.keySecondary,
+    };
+
+    public static implicit operator BymlArrayChangelogEntry((int index, BymlChangeType change, Byml node) src) => new() {
+        Index = src.index,
+        Change = src.change,
+        Node = src.node
     };
 
     public void Deconstruct(out int index, out BymlChangeType change, out Byml node, out Byml? keyPrimary, out Byml? keySecondary)
